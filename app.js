1251e7944d8c8cf9d6163de4441e2500ae0215d5
UrielBm <urielbenitez96@hotmail.com>
2023-01-29 23:14:41 -0600

    hotfix:cors update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: process.env.APP,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
